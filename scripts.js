b1831e5631a98814b6dff8df719c33ec33c179bc
FelipeFidelisA <146492055+FelipeFidelisA@users.noreply.github.com>
2024-08-24 14:19:43 -0300

    pedido

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
@@ -6,3 +5,21 @@ onload = () => {
     }, 1000);
   };
   
+  var moveButton = document.querySelector("button:nth-child(2)");
+  var correctlyButton = document.querySelector("button:first-child");
+  
+  moveButton.addEventListener("click", function () {
+    var maxVertical = window.innerWidth - moveButton.offsetWidth;
+    var maxHorizontal = window.innerHeight - moveButton.offsetHeight;
+    
+    var vertical = Math.floor(Math.random() * maxVertical);
+    var horizontal = Math.floor(Math.random() * maxHorizontal);
+    
+    moveButton.style.position = "absolute";
+    moveButton.style.left = vertical + "px";
+    moveButton.style.top = horizontal + "px";
+  });
+  
+  correctlyButton.addEventListener("click", function () {
+    alert("Resposta correta! Vamo comer um empadão");
+  });
